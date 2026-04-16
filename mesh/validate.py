5e6c53a2fe1196747c7a4e98d124936da9abf4d8
OSO1111 <84886092+OSO1111@users.noreply.github.com>
2026-04-16 16:31:37 +0900

    Update validate.py

--- mesh/validate.py
+++ mesh/validate.py
@@ -1,1 +1,12 @@
-dd
+import sys
+import trimesh
+import json
+
+mesh = trimesh.load(sys.argv[1])
+
+result = {
+    "printable": mesh.is_watertight,
+    "faces": len(mesh.faces)
+}
+
+print(json.dumps(result))
