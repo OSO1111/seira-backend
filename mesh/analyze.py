c0bea845f9fe8f179acebde1e81faab8a2f1acdf
OSO1111 <84886092+OSO1111@users.noreply.github.com>
2026-04-16 16:31:04 +0900

    Update analyze.py

--- mesh/analyze.py
+++ mesh/analyze.py
@@ -1,1 +1,14 @@
-print("analyze")
+import sys
+import trimesh
+import json
+
+mesh = trimesh.load(sys.argv[1])
+
+result = {
+    "is_watertight": mesh.is_watertight,
+    "faces": len(mesh.faces),
+    "vertices": len(mesh.vertices),
+    "components": len(mesh.split())
+}
+
+print(json.dumps(result))
